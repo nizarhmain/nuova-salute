fd59c7779bf975af0d79d8a2050814a6194feaa2
Nizar Hmain <hmain.nizar@gmail.com>
2025-12-07 17:10:42 +0700

    DEPLOY: try gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://nizarhmain.github.io',
+  base: '/nuova-salute',
   integrations: [mdx(), sitemap()],
 
   vite: {
